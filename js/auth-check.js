1bdc71d0444874afef222e1754970ffe23a3f910
projectbyd <project.by.deee@gmail.com>
2026-01-15 09:52:43 +0700

    Update auth-check.js

--- js/auth-check.js
+++ js/auth-check.js
@@ -1,3 +1,24 @@
+(function(){
+  let devtoolsOpen = false;
+
+  const threshold = 160;
+
+  setInterval(() => {
+    const widthDiff = window.outerWidth - window.innerWidth;
+    const heightDiff = window.outerHeight - window.innerHeight;
+
+    if (widthDiff > threshold || heightDiff > threshold) {
+      if (!devtoolsOpen) {
+        devtoolsOpen = true;
+        alert("Akses diblokir");
+        location.href = "login.html";
+      }
+    } else {
+      devtoolsOpen = false;
+    }
+  }, 1000);
+})();
+
 if (localStorage.getItem("sb_auth") !== "ok") {
   window.location.href = "login.html";
 }
